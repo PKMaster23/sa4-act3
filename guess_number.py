63fd9592d36e5b5eec5384999b4cd32071180cdb
Luke Freeman <mlfreeman2004@gmail.com>
2024-02-06 20:24:54 -0600

    Refactored number guesser to include a loop

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,18 @@
-number = 10
+number = '10'
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
+guess = input("What number am I thinking of? ")
+
+while guess != number:
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+    elif guess == 'q':
+        break
+    else:
+        print(f"Sorry! The number is not {guess}. Try again!")
+        guess = input("What number am I thinking of? ")
 
 if guess == number:
-   print("Congratulations! You guessed the right number.")
-else:
-   print(f"Sorry! The number was {number}.")
+    print("Congratulations! You guessed the right number.")
+if guess == 'q':
+    print('See you next time!')
