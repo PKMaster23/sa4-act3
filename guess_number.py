9b66044eedb16a0fd895e1910232ede56e6aad32
Luke Freeman <mlfreeman2004@gmail.com>
2024-02-06 20:38:06 -0600

    Implemented hints into the number guesser

--- guess_number.py
+++ guess_number.py
@@ -1,18 +1,21 @@
-number = '10'
+number = 10
 
 print("I'm thinking of a number...")
 guess = input("What number am I thinking of? ")
 
-while guess != number:
+while guess != 'q':
+    
+    guess = int(guess)
+
     if guess == number:
         print("Congratulations! You guessed the right number.")
-    elif guess == 'q':
         break
-    else:
-        print(f"Sorry! The number is not {guess}. Try again!")
-        guess = input("What number am I thinking of? ")
+    elif guess < number:
+        print("Sorry! Your guess is too low. Try again!")
+    elif guess > number:
+        print("Sorry! Your guess was too high. Try again!")
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
+    guess = input("What number am I thinking of? ")
+
 if guess == 'q':
     print('See you next time!')
