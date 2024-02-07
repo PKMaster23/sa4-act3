74ea21b79c651990f6ee9509c3e85b746340600f
Luke Freeman <mlfreeman2004@gmail.com>
2024-02-06 20:52:40 -0600

    Added a limited number of guesses

--- guess_number.py
+++ guess_number.py
@@ -1,18 +1,23 @@
-number = '10'
+number = 10
+
+i = 3
 
 print("I'm thinking of a number...")
 guess = input("What number am I thinking of? ")
 
-while guess != number:
+while guess != 'q':
+    guess = int(guess)
+
     if guess == number:
         print("Congratulations! You guessed the right number.")
-    elif guess == 'q':
+        break
+    elif i == 0:
+        print(f"Sorry, but the number was {number}.")
         break
     else:
-        print(f"Sorry! The number is not {guess}. Try again!")
-        guess = input("What number am I thinking of? ")
+        print(f"Sorry, that's not the right number. You have {i:.0f} attempts remaining.")
+        i = i - 1
+    guess = input("What number am I thinking of? ")
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
 if guess == 'q':
     print('See you next time!')
